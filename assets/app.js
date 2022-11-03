7efd969914de39754af714ddd2c3e91165ea9ceb
Víctor Iglesias Castán <victor.iglesias@dgtls.com>
2022-11-03 21:23:50 +0100

    Código hasta el capítulo 5 (incluido)

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,19 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
-
+import getNiceMessage from './get_nice_message';
+import $ from 'jquery';
+//global.$ = $;
+import 'bootstrap';
 // start the Stimulus application
 import './bootstrap';
+
+console.log(getNiceMessage(6));
+
+$('.dropdown-toggle').dropdown();
+$('.custom-file-input').on('change', function(event) {
+    var inputFile = event.currentTarget;
+    $(inputFile).parent()
+        .find('.custom-file-label')
+        .html(inputFile.files[0].name);
+});
