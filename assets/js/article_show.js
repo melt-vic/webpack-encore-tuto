1b1cad9b98825c2417b978c47f719feb69f9ac6d
Víctor Iglesias Castán <victor.iglesias@dgtls.com>
2022-12-28 12:55:54 +0100

    Código hasta el capítulo 19 (incluido)

--- assets/js/article_show.js
+++ assets/js/article_show.js
@@ -1,7 +1,11 @@
 import '../css/article_show.scss';
 import $ from 'jquery';
-
+//import 'bootstrap'; // Los módulos no se comparten entre entires (cada .addEntry en webpack.config.js ). Aunque haya un import de Bootstrap
+                    // en jquery que extiende las funcionalidades de jQuery, este entry no lo tiene, por lo que se debe volver a importar.
+// Si en webpack.config.js se activa .enableSingleRuntimeChunk() esto no es así sino que los módulos se comparten entre los entrypoints
 $(document).ready(function() {
+    $('.js-like-article').tooltip();
+
     $('.js-like-article').on('click', function(e) {
         e.preventDefault();
 
